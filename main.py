d472a5c1e3b655e0ee84ba9a04e0bcd8c452ee96
majkeloangelo <michal.szczechowicz@outlook.com>
2023-11-20 22:58:21 +0100

    update 1.15 - added table to the layout - added query to sum listened time - added adding values to table(still not working properly)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import sys
 import psycopg2
 
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QStandardItemModel
 from PyQt5.QtWidgets import *
 
 class Window(QWidget):
@@ -11,7 +11,7 @@ class Window(QWidget):
 
         #set basic config
         self.setWindowTitle('Spotify Analyzer')
-        self.setGeometry(0, 0, 600, 400)
+        self.setGeometry(0, 0, 800, 600)
 
         layout = QGridLayout()
 
@@ -23,35 +23,49 @@ class Window(QWidget):
         label.setPixmap(image)
 
         #load file
-        load_button = QPushButton('search from computer')
+        load_button = QPushButton('Search from computer')
         load_button.clicked.connect(self.load_file)
 
         #set table
-        #data = {'id':[],
-                #'Artist name':[1],
-                #'Track name':[1],
-                #'Time played':[1]}
-
         self.table = QTableWidget(self)
-        self.table.setColumnCount(4)
-        self.table.setColumnWidth(0, 50)
+        self.table.setColumnCount(3)
+        self.table.setHorizontalHeaderLabels(["Artist name", "Track name", "Time played"])
+        self.table.setColumnWidth(0, 150)
         self.table.setColumnWidth(1, 150)
         self.table.setColumnWidth(2, 150)
-        self.table.setColumnWidth(3, 150)
+        self.table.setRowCount(5)
+        header = self.table.horizontalHeader()
+        header.setSectionResizeMode(0, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(2, QHeaderView.ResizeMode.ResizeToContents)
+        #self.table.setItem(0, 0, QTableWidgetItem(self.get_data(0)))
+        #self.table.setItem(1, 0, QTableWidgetItem(self.get_data(1)))
+        #self.table.setItem(2, 0, QTableWidgetItem(self.get_data(2)))
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+        for i in range (5):
+            loaded = self.get_data(i)
+            for j in loaded:
+                for t in range(3):
+                    self.table.setItem(i, t, QTableWidgetItem(j[t]))
+
+
 
         #set widgets to the grid
         layout.addWidget(label, 0, 0)
         layout.addWidget(QLabel('My Spotify Data Analyze'), 0 ,1)
         layout.addWidget(QLabel('Load data:'), 1, 0)
         layout.addWidget((load_button), 1, 1)
-        layout.addWidget(QLabel('Your data:'), 2, 0)
+        layout.addWidget(QLabel('Your top 5 favorite songs listened on Spotify:'), 2, 0)
         layout.addWidget(self.table, 3, 0)
 
         self.setLayout(layout)
 
+
+
+
     #load file - next function wich will part json into chunks
     def load_file(self):
-        options = QFileDialog.Options()
         options = QFileDialog.DontUseNativeDialog
         file_name, ok = QFileDialog.getOpenFileName(self, "Select file", "", "*.json", options=options)
 
@@ -103,6 +117,28 @@ class Window(QWidget):
             conn.commit()
             cur.close()
             conn.close()
+
+        except (Exception, psycopg2.DatabaseError):
+            self.message_box("Database connection error.")
+    def get_data(self, index):
+        try:
+            conn = psycopg2.connect(
+                host="localhost",
+                database="spoify_data",
+                user="postgres",
+                password="qwerty123")
+            cur = conn.cursor()
+
+            query = ("SELECT artist_name, track_name, SUM(ms_played) as czas_sluchania FROM data GROUP BY track_name, artist_name ORDER BY czas_sluchania DESC LIMIT 1 OFFSET {};".format(index))
+
+            cur.execute(query)
+            data = cur.fetchall()
+
+            conn.commit()
+            cur.close()
+            conn.close()
+
+            return data
 
         except (Exception, psycopg2.DatabaseError):
             self.message_box("Database connection error.")
